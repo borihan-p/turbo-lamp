ae205a0b3367eef78ef126bb1026ed8f8467bccd
borihan <borihan@yahoo.com>
2023-03-29 15:45:47 +0700

    add hello3

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
         <p>
          Hello2 Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p>
+         Hello3 Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
